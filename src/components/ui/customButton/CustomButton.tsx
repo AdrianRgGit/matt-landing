d48473256eb87030d25fed8ad6631a8026096f87
aramirez-neural <aramirez@neural.es>
2026-03-26 17:28:31 +0100

    hero tirado.

---
+++ src/components/ui/customButton/CustomButton.tsx
@@ -0,0 +1,27 @@
+type ButtonProps = {
+  text?: string;
+  onClick?: () => void;
+  className?: string;
+};
+
+export default function Button({
+  text = "Partner with us",
+  onClick,
+  className = "",
+}: ButtonProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`flex cursor-pointer group ${className}`}
+    >
+      <div className="bg-theme-blue w-fit p-4 rounded-lg group-hover:bg-theme-blue/80 transition-colors duration-200">
+        <span className="font-spacegrotesk-light text-font-white">{text}</span>
+      </div>
+
+      <div className="border-4 border-theme-blue rounded-lg w-12 flex items-center justify-center">
+        f
+      </div>
+    </button>
+  );
+}
