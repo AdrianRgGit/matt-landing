943fba5075a6743e55f9065385979a17b09ba371
aramirez-neural <aramirez@neural.es>
2026-04-08 11:01:50 +0200

    eliminado la importación de react de los inputs.

--- src/components/ui/customInput/CustomInput.tsx
+++ src/components/ui/customInput/CustomInput.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type CustomInputProps = {
   label: string;
   name: string;
--- src/components/ui/customTextarea/CustomTextarea.tsx
+++ src/components/ui/customTextarea/CustomTextarea.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type CustomTextareaProps = {
   label: string;
   name: string;
